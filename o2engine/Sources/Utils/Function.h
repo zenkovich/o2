94a28d03aad8a171b445353b48f06fa84eb6fdc8
Andrey <zenkovichan@gmail.com>
2015-04-14 02:37:44 +0600

    Add TFunction class

--- o2engine/Sources/Utils/Function.h
+++ o2engine/Sources/Utils/Function.h
@@ -8,115 +8,388 @@ namespace o2
 {
 #define Function std::function
 
-	template<class T>
-	class FunctionsList
-	{
-	protected:
-		typedef std::vector< Function<T> > FunctionsVec;
+	template <typename UnusedType>
+	class TFunction;
 
-		FunctionsVec mFunctions;
+	template<typename _res_type, typename ... _args>
+	class TFunction <_res_type(_args ...)>
+	{
+		struct IFuncInvoker
+		{
+			enum class InvokerType { Static, Object };
+
+			virtual IFuncInvoker* Clone() const = 0;
+			virtual _res_type Invoke(_args ... args) = 0;
+			virtual InvokerType GetType() const = 0;
+			virtual bool Equals(IFuncInvoker* other) const = 0;
+		};
+		typedef std::vector<IFuncInvoker*> InvokersVec;
+
+		template<typename _func_type>
+		struct StaticFuncInvoker: public IFuncInvoker
+		{
+			_func_type* mFunction;
+
+			StaticFuncInvoker(_func_type* func):
+				mFunction(func)
+			{
+			}
+
+			IFuncInvoker* Clone() const
+			{
+				return new StaticFuncInvoker(mFunction);
+			}
+
+			_res_type Invoke(_args ... args)
+			{
+				return (*mFunction)(args ...);
+			}
+
+			InvokerType GetType() const
+			{
+				return IFuncInvoker::InvokerType::Static;
+			}
+
+			bool Equals(IFuncInvoker* other) const
+			{
+				if (other->GetType() != InvokerType::Static)
+					return false;
+
+				StaticFuncInvoker* otherStatic = (StaticFuncInvoker*)other;
+				return mFunction == otherStatic->mFunction;
+			}
+		};
+
+		template <typename _func_type, typename _class_type>
+		struct ObjectFuncInvoker: public IFuncInvoker
+		{
+			typedef _func_type _class_type::* ObjectFuncSignature;
+
+			ObjectFuncSignature mFunction;
+			_class_type*        mObject;
+
+			ObjectFuncInvoker(ObjectFuncSignature func, _class_type* object):
+				mFunction(func), mObject(object)
+			{
+			}
+
+			IFuncInvoker* Clone() const
+			{
+				return new ObjectFuncInvoker(mFunction, mObject);
+			}
+
+			_res_type Invoke(_args ... args)
+			{
+				return (mObject->*mFunction)(args ...);
+			}
+
+			InvokerType GetType() const
+			{
+				return IFuncInvoker::InvokerType::Object;
+			}
+
+			bool Equals(IFuncInvoker* other) const
+			{
+				if (other->GetType() != InvokerType::Object)
+					return false;
+
+				ObjectFuncInvoker* otherObject = (ObjectFuncInvoker*)other;
+				return mFunction == otherObject->mFunction && mObject == otherObject->mObject;
+			}
+		};
+
+		InvokersVec mInvokers;
 
 	public:
-		FunctionsList();
-		FunctionsList(const Function<T>& func);
+		TFunction()
+		{
+		}
 
-		void Add(const FunctionsList<T>& other);
-		int  Add(const Function<T>& func);
-		void Remove(int idx);
-		void Clear();
+		TFunction(const TFunction& other)
+		{
+			Add(other);
+		}
 
-		FunctionsList& operator=(const Function<T>& func);
-		FunctionsList  operator+(const Function<T>& func) const;
-		FunctionsList& operator+=(const Function<T>& func);
-		FunctionsList  operator+(const FunctionsList<T>& other) const;
-		FunctionsList& operator+=(const FunctionsList<T>& other);
+		template<typename _func_type>
+		TFunction(_func_type* func)
+		{
+			mInvokers.push_back(new StaticFuncInvoker<_func_type>(func));
+		}
 
-		template<class... Args>
-		void operator()(Args... args);
+		template <typename _class_type>
+		TFunction(_res_type(_class_type::*func)(_args...), _class_type& object)
+		{
+			mInvokers.push_back(new ObjectFuncInvoker<_res_type(_args ...), _class_type>(func, &object));
+		}
+
+		~TFunction()
+		{
+			Clear();
+		}
+
+		void Clear()
+		{
+			for (auto invoker:mInvokers)
+				delete invoker;
+
+			mInvokers.clear();
+		}
+
+		template<typename _func_type>
+		void Add(_func_type* func)
+		{
+			mInvokers.push_back(new StaticFuncInvoker<_func_type>(func));
+		}
+
+		template <typename _class_type>
+		void Add(_res_type(_class_type::*func)(_args...), _class_type& object)
+		{
+			mInvokers.push_back(new ObjectFuncInvoker<_res_type(_args ...), _class_type>(func, &object));
+		}
+
+		void Add(const TFunction& other)
+		{
+			for (auto inv:other.mInvokers)
+				mInvokers.push_back(inv->Clone());
+		}
+
+		template<typename _func_type>
+		void Remove(_func_type* func)
+		{
+			for (auto invokerIt = mInvokers.begin(); invokerIt != mInvokers.end(); ++invokerIt)
+			{
+				if ((*invokerIt)->GetType() == IFuncInvoker::InvokerType::Static)
+				{
+					StaticFuncInvoker<_func_type>* invStatic = (StaticFuncInvoker<_func_type>*)(*invokerIt);
+					if (func == invStatic->mFunction)
+					{
+						mInvokers.erase(invokerIt);
+						return;
+					}
+				}
+			}
+		}
+
+		template <typename _class_type>
+		void Remove(_res_type(_class_type::*func)(_args...), _class_type& object)
+		{
+			for (auto invokerIt = mInvokers.begin(); invokerIt != mInvokers.end(); ++invokerIt)
+			{
+				if ((*invokerIt)->GetType() == IFuncInvoker::InvokerType::Object)
+				{
+					ObjectFuncInvoker<_res_type(_args ...), _class_type>* invObject =
+						(ObjectFuncInvoker<_res_type(_args ...), _class_type>*)(*invokerIt);
+
+					if (func == invObject->mFunction && &object == invObject->mObject)
+					{
+						mInvokers.erase(invokerIt);
+						return;
+					}
+				}
+			}
+		}
+
+		void Remove(const TFunction<_res_type(_args ...)>& other)
+		{
+			for (auto otherInvoker:other.mInvokers)
+			{
+				for (auto invokerIt = mInvokers.begin(); invokerIt != mInvokers.end(); ++invokerIt)
+				{
+					if ((*invokerIt)->Equals(otherInvoker))
+					{
+						mInvokers.erase(invokerIt);
+						break;
+					}
+				}
+			}
+		}
+
+		template<typename _func_type>
+		bool Contains(_func_type* func) const
+		{
+			for (auto invoker:mInvokers)
+			{
+				if (invoker->GetType() == IFuncInvoker::InvokerType::Static)
+				{
+					StaticFuncInvoker<_func_type>* invStatic = (StaticFuncInvoker<_func_type>*)invoker;
+					if (func == invStatic->mFunction)
+						return true;
+				}
+			}
+
+			return false;
+		}
+
+		template <typename _class_type>
+		bool Contains(_res_type(_class_type::*func)(_args...), _class_type& object) const
+		{
+			for (auto invoker:mInvokers)
+			{
+				if (invoker->GetType() == IFuncInvoker::InvokerType::Object)
+				{
+					ObjectFuncInvoker<_res_type(_args ...), _class_type>* invObject =
+						(ObjectFuncInvoker<_res_type(_args ...), _class_type>*)invoker;
+
+					if (func == invObject->mFunction && &object == invObject->mObject)
+						return true;
+				}
+			}
+
+			return false;
+		}
+
+		bool Contains(const TFunction& other) const
+		{
+			for (auto otherInvoker:other.mInvokers)
+			{
+				bool found = false;
+				for (auto invoker:mInvokers)
+				{
+					if (invoker->Equals(otherInvoker))
+					{
+						found = true;
+						break;
+					}
+				}
+
+				if (!found)
+					return false;
+			}
+
+			return true;
+		}
+
+		_res_type operator()(_args ... args)
+		{
+			if (mInvokers.size() == 0)
+				return _res_type();
+
+			auto it = mInvokers.begin();
+			for (; it != mInvokers.end() - 1; ++it)
+				(*it)->Invoke(args ...);
+
+			return (*it)->Invoke(args ...);
+		}
+
+		template<typename _func_type>
+		TFunction<_res_type(_args ...)>& operator=(_func_type* func)
+		{
+			Clear();
+			Add(func);
+			return *this;
+		}
+
+		TFunction<_res_type(_args ...)>& operator=(const TFunction& other)
+		{
+			Clear();
+			Add(other);
+			return *this;
+		}
+
+		bool operator==(const TFunction& other) const
+		{
+			for (auto invoker:mInvokers)
+			{
+				bool found = false;
+				for (auto otherInvoker:other.mInvokers)
+				{
+					if (invoker->Equals(otherInvoker))
+					{
+						found = true;
+						break;
+					}
+				}
+
+				if (!found)
+					return false;
+			}
+
+			return true;
+		}
+
+		bool operator!=(const TFunction& other) const
+		{
+			return !(*this == other);
+		}
+
+		template<typename _func_type>
+		bool operator==(_func_type* func) const
+		{
+			if (mInvokers.size() != 1)
+				return false;
+
+			IFuncInvoker* invoker = mInvokers[0];
+			if (invoker->GetType() != IFuncInvoker::InvokerType::Static)
+				return false;
+
+			StaticFuncInvoker<_func_type>* invStatic = (StaticFuncInvoker<_func_type>*)invoker;
+			if (func != invStatic->mFunction)
+				return false;
+
+			return true;
+		}
+
+		template<typename _func_type>
+		bool operator!=(_func_type* func) const
+		{
+			return !(*this == func);
+		}
+
+		template<typename _func_type>
+		TFunction<_res_type(_args ...)> operator+(_func_type* func)
+		{
+			TFunction<_res_type(_args ...)> res(*this);
+			res.Add(func);
+			return res;
+		}
+
+		template<typename _func_type>
+		TFunction<_res_type(_args ...)>& operator+=(_func_type* func)
+		{
+			Add(func);
+			return *this;
+		}
+
+		TFunction<_res_type(_args ...)> operator+(const TFunction& other)
+		{
+			TFunction<_res_type(_args ...)> res(*this);
+			res.Add(other);
+			return res;
+		}
+
+		TFunction<_res_type(_args ...)> operator+=(const TFunction& other)
+		{
+			Add(other);
+			return *this;
+		}
+
+
+		template<typename _func_type>
+		TFunction<_res_type(_args ...)> operator-(_func_type* func)
+		{
+			TFunction<_res_type(_args ...)> res(*this);
+			res.Remove(func);
+			return res;
+		}
+
+		template<typename _func_type>
+		TFunction<_res_type(_args ...)>& operator-=(_func_type* func)
+		{
+			Remove(func);
+			return *this;
+		}
+
+		TFunction<_res_type(_args ...)> operator-(const TFunction& other)
+		{
+			TFunction<_res_type(_args ...)> res(*this);
+			res.Remove(other);
+			return res;
+		}
+
+		TFunction<_res_type(_args ...)> operator-=(const TFunction& other)
+		{
+			Remove(other);
+			return *this;
+		}
 	};
-
-	//implementation
-	template<class T>
-	FunctionsList<T>::FunctionsList() {}
-
-	template<class T>
-	FunctionsList<T>::FunctionsList(const Function<T>& func)
-	{
-		mFunctions.push_back(func);
-	}
-
-	template<class T>
-	void FunctionsList<T>::Add(const FunctionsList<T>& other)
-	{
-		for (auto fn:other.mFunctions)
-			mFunctions.push_back(func);
-	}
-
-	template<class T>
-	int FunctionsList<T>::Add(const Function<T>& func)
-	{
-		mFunctions.push_back(func);
-		return mFunctions.size() - 1;
-	}
-
-	template<class T>
-	void FunctionsList<T>::Remove(int idx)
-	{
-		if (idx < 0 || idx >= mFunctions.size())
-			return;
-
-		mFunctions.erase(mFunctions.begin() + idx);
-	}
-
-	template<class T>
-	void FunctionsList<T>::Clear()
-	{
-		mFunctions.clear();
-	}
-
-	template<class T>
-	FunctionsList<T>& FunctionsList<T>::operator=(const Function<T>& func)
-	{
-		mFunctions.clear();
-		mFunctions.push_back(func);
-		return *this;
-	}
-
-	template<class T>
-	FunctionsList<T> FunctionsList<T>::operator+(const Function<T>& func) const
-	{
-		FunctionsList<T> res(*this);
-		res.Add(func);
-		return res;
-	}
-
-	template<class T>
-	FunctionsList<T>& FunctionsList<T>::operator+=(const Function<T>& func)
-	{
-		Add(func);
-		return *this;
-	}
-
-	template<class T>
-	FunctionsList<T> FunctionsList<T>::operator+(const FunctionsList<T>& other) const
-	{
-		FunctionsList<T> res(*this);
-		res.Add(other);
-		return res;
-	}
-
-	template<class T>
-	FunctionsList<T>& FunctionsList<T>::operator+=(const FunctionsList<T>& other)
-	{
-		Add(other);
-		return *this;
-	}
-
-	template<class T>
-	template<class... Args>
-	void FunctionsList<T>::operator()(Args... args)
-	{
-		for (auto fn:mFunctions)
-			fn(args...);
-	}
 }
--- o2engine/tests/Sources/o2tests.cpp
+++ o2engine/tests/Sources/o2tests.cpp
@@ -14,259 +14,77 @@
 
 struct TestStruct
 {
-	int a, b;
-	float d;
-	String c;
+	int a;
 
-	TestStruct(){}
-	TestStruct(const String& f) :c(f), a(10) {}
+	TestStruct():a(10) {}
 
-	bool operator==(const TestStruct& other) const
-	{
-		return a == other.a && b == other.b && d == other.d && c == other.c;
-	}
-};
-
-struct TestStructArr
-{
-	Array<int> arr;
-
-	int FindMax() const
-	{
-		int res = 0;
-		for (auto n : arr)
-		{
-			if (n > res)
-				res = n;
-		}
-
-		return res;
-	}
-
-	void print()
-	{
-		printf("==========\n");
-		for (auto n : arr)
-			printf("%i\n", n);
-	}
-
-	template<typename T>
-	void TemplTest(const Function<T(int)> fn)
-	{
-		int rr = (int)(fn(10));
-		printf("rr = %i\n", rr);
-	}
-};
-
-template <typename UnusedType>
-class function;
-
-
-template <typename ReturnType, typename ... ArgumentTypes>
-class function <ReturnType(ArgumentTypes ...)>
-{
-public:
-
-	typedef ReturnType signature_type(ArgumentTypes ...);
-
-	function()
-		: mInvoker()
-	{
-	}
-
-
-	template <typename FunctionT>
-
-	/// Uncomment this, and add corresponding assignment operator for `check4`
-	///explicit
-	function(FunctionT f)
-		: mInvoker(new free_function_holder<FunctionT>(f))
-	{
-	}
-
-	template <typename FunctionType, typename ClassType>
-	function(FunctionType ClassType::* f)
-		: mInvoker(new member_function_holder<FunctionType, ArgumentTypes ...>(f))
-	{
-	}
-
-
-	function(const function & other)
-		: mInvoker(other.mInvoker->clone())
-	{
-	}
-
-	function & operator = (const function & other)
-	{
-		mInvoker = other.mInvoker->clone();
-	}
-
-	ReturnType operator ()(ArgumentTypes ... args)
-	{
-		return mInvoker->invoke(args ...);
-	}
-
-
-private:
-
-
-	class function_holder_base
-	{
-	public:
-
-		function_holder_base()
-		{
-		}
-
-		virtual ~function_holder_base()
-		{
-		}
-
-		virtual ReturnType invoke(ArgumentTypes ... args) = 0;
-
-		virtual std::auto_ptr<function_holder_base> clone() = 0;
-
-	private:
-		function_holder_base(const function_holder_base &);
-		void operator = (const function_holder_base &);
-	};
-
-
-	typedef std::auto_ptr<function_holder_base> invoker_t;
-
-
-	template <typename FunctionT>
-	class free_function_holder : public function_holder_base
-	{
-	public:
-
-		free_function_holder(FunctionT func)
-			: function_holder_base(),
-			mFunction(func)
-		{
-
-		}
-
-		virtual ReturnType invoke(ArgumentTypes ... args)
-		{
-			return mFunction(args ...);
-		}
-
-
-		virtual invoker_t clone()
-		{
-			return invoker_t(new free_function_holder(mFunction));
-		}
-
-	private:
-
-		FunctionT mFunction;
-	};
-
-
-	template <typename FunctionType, typename ClassType, typename ... RestArgumentTypes>
-	class member_function_holder : public function_holder_base
-	{
-	public:
-
-		typedef FunctionType ClassType::* member_function_signature_t;
-
-		member_function_holder(member_function_signature_t f)
-			: mFunction(f)
-		{}
-
-		virtual ReturnType invoke(ClassType obj, RestArgumentTypes ... restArgs)
-		{
-			return (obj.*mFunction)(restArgs ...);
-		}
-
-		virtual invoker_t clone()
-		{
-			return invoker_t(new member_function_holder(mFunction));
-		}
-
-	private:
-		member_function_signature_t mFunction;
-	};
-
-	invoker_t mInvoker;
+	void sum(int x) { printf("sum = %i\n", a + x); }
+	void mul(int x) { printf("mul = %i\n", a*x); }
 };
 
 void smFunc(int x)
 {
-	printf("%i\n", x);
+	printf("out %i\n", x);
 }
 
 int main(char** lpCmdLine, int nCmdShow)
 {
-	function<void(int)> tht = smFunc;
-	tht(5);
-
 	TestMath();
 
-	Debug::Log("Some log");
-	Debug::LogWarning("Some log warning");
-	Debug::LogError("Some log error");
+	TestStruct testStruct;
+	auto lambda = [](int x) { printf("lambda %i\n", x); };
+	TFunction<void(int)> func(smFunc);
+	func.Add(&TestStruct::mul, testStruct);
+	func.Add(&TestStruct::sum, testStruct);
+	func.Add(&lambda);
+	func(3);
 
-	auto xx = FormatStr("%f", 5.4f);
-	Debug::Log(xx);
+	printf("==========\n");
+	TFunction<void(int)> func2(smFunc);
+	func2.Add(&TestStruct::mul, testStruct);
+	func2.Remove(smFunc);
+	func.Remove(func2);
+	func(3);
 
-	Array<TestStruct> arrTest;
-	arrTest.Add(TestStruct("a"));
-	arrTest.Add(TestStruct("b"));
-	arrTest.Add(TestStruct("c"));
-	arrTest.Add(TestStruct("d"));
-	for (auto x : arrTest)
-	{
-		Debug::Log("%s", x.c.c_str());
-	}
+	printf("==========\n");
+	func = TFunction<void(int)>(&TestStruct::mul, testStruct);
+	func(3);
 
-	Debug::Log("=============");
+	printf("==========\n");
+	TFunction<void(int)> func3(func);
+	func3 += &lambda;
+	func3(3);
 
-	FunctionsList<void(int)> tt([](int x){ printf("%i\n", x + 1); });
-	auto tt2 = tt + [](int x){ printf("%i\n", x + 2); };
-	auto lamb = [](int x){ printf("%i\n", x + 3); };
-	tt2 += lamb;
+	printf("==========\n");
+	func3 -= &lambda;
+	func3(3);
 
-	tt2(10);
+	func3 = smFunc;
 
-	Debug::Log("=============");
+	if (func3 == smFunc)
+		printf("Equals\n");
 
-	//tt2 -= lamb;
-	tt2(10);
+	printf("==========\n");
+	TFunction<void(int)> func4(smFunc);
+	func4.Add(&TestStruct::mul, testStruct);
+	func4.Add(&TestStruct::sum, testStruct);
+	func4.Add(&lambda);
+	func4(3);
 
-	TestStructArr bbc;
-	bbc.arr.Add(0);
-	bbc.arr.Add(5);
-	bbc.arr.Add(2);
-	bbc.arr.Add(8);
-	bbc.arr.Add(11);
-	bbc.print();
-	printf("max is %i\n", bbc.FindMax());
+	if (func4.Contains(smFunc))
+		printf("Contains smFunc\n");
 
-	bbc.print();
+	if (func4.Contains(&TestStruct::mul, testStruct))
+		printf("Contains &TestStruct::mul, testStruct\n");
 
-	bbc.TemplTest<float>([](int x) { return (float)x; });
+	if (func4.Contains(&lambda))
+		printf("Contains lambda\n");
 
+	func4 -= smFunc;
 
-	Debug::Log("=============");
-	Array<float> farr;
-	farr.Add(0.1f);
-	farr.Add(0.01f);
-	farr.Add(0.6f);
-	farr.Add(0.4f);
-	farr.Add(0.1f);
-	farr.Add(0.18f);
-	farr.Add(2.1f);
-	farr.Add(3.1f);
+	if (func4.Contains(func3 + &lambda))
+		printf("Contains &lambda + smFunc\n");
 
-	farr.ForEach([](float x){ Debug::Log("%f", x); });
-	Debug::Log("=============");
-	farr.RemoveAll([](float x){ return x > 0.5f; });
-	farr.SortBy<int>([](float x){ return (int)(x*100.0f); });
-	farr.Take(3).ForEach([](float x){ Debug::Log("%f", x); });
-
-	Debug::Log("Sum: %f", farr.Sum<float>([](float ff){ return ff; }));
 
 	printf("All tests completed!");
 	_getch();
