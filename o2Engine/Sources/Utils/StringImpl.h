8e633a7373ad94cc0bba28e4dd1094525a3a309d
Andrey Zenkovich <zenkovichan@gmail.com>
2017-12-16 12:43:04 +0300

    Rectangle serialization fix

--- o2Engine/Sources/Utils/StringImpl.h
+++ o2Engine/Sources/Utils/StringImpl.h
@@ -543,7 +543,7 @@ namespace o2
 		splitted[2].Trim();
 		splitted[3].Trim();
 
-		return RectF((float)splitted[0], (float)splitted[1], (float)splitted[2], (float)splitted[3]);
+		return RectF((float)splitted[0], (float)splitted[3], (float)splitted[2], (float)splitted[1]);
 	}
 
 	template<typename T>
@@ -558,7 +558,7 @@ namespace o2
 		splitted[2].Trim();
 		splitted[3].Trim();
 
-		return RectI((int)splitted[0], (int)splitted[1], (int)splitted[2], (int)splitted[3]);
+		return RectI((int)splitted[0], (int)splitted[3], (int)splitted[2], (int)splitted[1]);
 	}
 
 	template<typename T>
@@ -573,7 +573,7 @@ namespace o2
 		splitted[2].Trim();
 		splitted[3].Trim();
 
-		return BorderF((float)splitted[0], (float)splitted[1], (float)splitted[2], (float)splitted[3]);
+		return BorderF((float)splitted[0], (float)splitted[3], (float)splitted[2], (float)splitted[1]);
 	}
 
 	template<typename T>
@@ -588,7 +588,7 @@ namespace o2
 		splitted[2].Trim();
 		splitted[3].Trim();
 
-		return BorderI((int)splitted[0], (int)splitted[1], (int)splitted[2], (int)splitted[3]);
+		return BorderI((int)splitted[0], (int)splitted[3], (int)splitted[2], (int)splitted[1]);
 	}
 
 	template<typename T>
