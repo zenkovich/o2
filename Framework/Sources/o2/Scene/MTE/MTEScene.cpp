9991532a984970ebe7790ff67addcbdb1b18526f
Zenkovich Andrey <zenkovichan@gmail.com>
2023-03-29 04:19:22 +0300

    Cmake and MTESceneActor WIP

--- Framework/Sources/o2/Scene/MTE/MTEScene.cpp
+++ Framework/Sources/o2/Scene/MTE/MTEScene.cpp
@@ -4,6 +4,7 @@
 #include "Box2D/Dynamics/b2Body.h"
 #include "o2/Physics/PhysicsWorld.h"
 #include "o2/Scene/Physics/ICollider.h"
+#include "o2/Scene/DrawableComponent.h"
 
 namespace o2
 {
@@ -20,7 +21,7 @@ namespace o2
 
 	MTESceneActor& MTESceneActor::operator=(const MTESceneActor& other)
 	{
-
+		Actor::operator=(other);
 		return *this;
 	}
 
@@ -28,7 +29,6 @@ namespace o2
 	void MTESceneActor::OnEnableInHierarchyChanged()
 	{
 		Actor::OnEnableInHierarchyChanged();
-
 	}
 
 	void MTESceneActor::OnAddToScene()
@@ -39,6 +39,75 @@ namespace o2
 	void MTESceneActor::OnRemoveFromScene()
 	{
 		Actor::OnRemoveFromScene();
+	}
+
+	void MTESceneActor::OnUpdate(float dt)
+	{
+		m_scene->update(dt);
+	}
+
+	void MTESceneActor::Draw()
+	{
+		m_scene->draw();
+	}
+
+	void MTESceneActor::Initialize(SceneFactory sceneFactory, std::string_view options)
+	{
+		m_render = mte::makeRenderer();
+		m_video = mte::make_video_factory();
+		m_fileserver = mte::make_fileserver();
+		m_events = mte::make_event_manager();
+		mte::ResourceSubsystem subsystems{ m_video.get(), m_fileserver.get(), m_render.get() };
+		m_scene = sceneFactory(m_events.get(), subsystems, options);
+
+		m_render->init();
+		m_scene->init();
+		m_scene->attach();
+
+		Vec2F renderSize = o2Render.GetCurrentResolution();
+		std::int32_t width = (std::int32_t)renderSize.x;
+		std::int32_t height = (std::int32_t)renderSize.y;
+		m_scene->setViewport(width, height);
+		m_render->setViewport({ 0, 0, width, height });
+	}
+
+	SceneLayer* MTESceneActor::GetSceneDrawableSceneLayer() const
+	{
+		return mLayer;
+	}
+
+	bool MTESceneActor::IsSceneDrawableEnabled() const
+	{
+		return mResEnabledInHierarchy;
+	}
+
+	ISceneDrawable* MTESceneActor::GetParentDrawable()
+	{
+		if (mParent)
+		{
+			auto itParent = mParent;
+			while (itParent)
+			{
+				if (auto drawable = dynamic_cast<ISceneDrawable*>(itParent))
+					return drawable;
+
+				auto comp = itParent->GetComponent<DrawableComponent>();
+				if (comp)
+					return (ISceneDrawable*)comp;
+
+				itParent = itParent->GetParent();
+			}
+		}
+
+		return nullptr;
+	}
+
+	int MTESceneActor::GetIndexInParentDrawable() const
+	{
+		if (mParent)
+			return mParent->GetChildren().IndexOf((Actor*)this);
+
+		return 0;
 	}
 }
 
--- Framework/Sources/o2/Scene/MTE/MTEScene.h
+++ Framework/Sources/o2/Scene/MTE/MTEScene.h
@@ -1,11 +1,21 @@
 #pragma once
 
 #include "o2/Scene/Actor.h"
+#include "o2/Scene/ISceneDrawable.h"
+
+#include "gengine/renderer/renderer_fwd.h"
+#include "gengine/renderer/renderer.h"
+#include "gengine/graphics/definition.h"
+#include "gengine/scene/scene.h"
 
 namespace o2
 {
-	class MTESceneActor : public Actor
+	class MTESceneActor : public Actor, public ISceneDrawable
 	{
+	public:
+		using SceneFactory = std::function<std::shared_ptr<mte::Scene>(
+			mte::EventManager* manager, mte::ResourceSubsystem subsystems, std::string_view options)>;
+
 	public:
 		// Default constructor
 		MTESceneActor();
@@ -19,9 +29,24 @@ namespace o2
 		// Destructor
 		~MTESceneActor();
 
+		// Initializes actor with scene factory
+		void Initialize(SceneFactory sceneFactory, std::string_view options);
+
+		// Draws content of scene
+		void Draw() override;
+
 		SERIALIZABLE(MTESceneActor);
 
 	protected:
+		int m_windowWidth;
+		int m_windowHeight;
+
+		mte::RendererPtr m_render;
+		mte::VideoFactoryPtr m_video;
+		mte::FileServerPtr m_fileserver;
+		mte::EventManagerPtr m_events;
+
+		std::shared_ptr<mte::Scene> m_scene;
 
 	protected:
 		// Called when result enable was changed
@@ -32,16 +57,39 @@ namespace o2
 
 		// Called when actor has removed from scene; destroys rigid body
 		void OnRemoveFromScene() override;
+
+		// Called on update with frame dt
+		void OnUpdate(float dt) override;
+
+		// Returns current scene layer
+		SceneLayer* GetSceneDrawableSceneLayer() const override;
+
+		// Returns is drawable enabled
+		bool IsSceneDrawableEnabled() const override;
+
+		// Returns parent scene drawable
+		ISceneDrawable* GetParentDrawable() override;
+
+		// Returns the index in the parent's list of children, used to sort the rendering
+		int GetIndexInParentDrawable() const override;
 	};
 }
 
 CLASS_BASES_META(o2::MTESceneActor)
 {
 	BASE_CLASS(o2::Actor);
+	BASE_CLASS(o2::ISceneDrawable);
 }
 END_META;
 CLASS_FIELDS_META(o2::MTESceneActor)
 {
+	FIELD().PROTECTED().NAME(m_windowWidth);
+	FIELD().PROTECTED().NAME(m_windowHeight);
+	FIELD().PROTECTED().NAME(m_render);
+	FIELD().PROTECTED().NAME(m_video);
+	FIELD().PROTECTED().NAME(m_fileserver);
+	FIELD().PROTECTED().NAME(m_events);
+	FIELD().PROTECTED().NAME(m_scene);
 }
 END_META;
 CLASS_METHODS_META(o2::MTESceneActor)
@@ -49,8 +97,15 @@ CLASS_METHODS_META(o2::MTESceneActor)
 
 	FUNCTION().PUBLIC().CONSTRUCTOR();
 	FUNCTION().PUBLIC().CONSTRUCTOR(const MTESceneActor&);
+	FUNCTION().PUBLIC().SIGNATURE(void, Initialize, SceneFactory, std::string_view);
+	FUNCTION().PUBLIC().SIGNATURE(void, Draw);
 	FUNCTION().PROTECTED().SIGNATURE(void, OnEnableInHierarchyChanged);
 	FUNCTION().PROTECTED().SIGNATURE(void, OnAddToScene);
 	FUNCTION().PROTECTED().SIGNATURE(void, OnRemoveFromScene);
+	FUNCTION().PROTECTED().SIGNATURE(void, OnUpdate, float);
+	FUNCTION().PROTECTED().SIGNATURE(SceneLayer*, GetSceneDrawableSceneLayer);
+	FUNCTION().PROTECTED().SIGNATURE(bool, IsSceneDrawableEnabled);
+	FUNCTION().PROTECTED().SIGNATURE(ISceneDrawable*, GetParentDrawable);
+	FUNCTION().PROTECTED().SIGNATURE(int, GetIndexInParentDrawable);
 }
 END_META;
